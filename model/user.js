f148d55469e78cd061652f91c6f89df08785a5b8
Vru007 <vrushik7143work@gmail.com>
2024-07-13 01:25:16 +0530

    final_dist

--- model/user.js
+++ model/user.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 const {Schema}=mongoose;
 const userSchema=new Schema({
-    name:{type:String,required:true,},
+    name:{type:String,required:false},
     email: {type:String,required:true,unique:true},
     password:{type:Buffer,required:true},
     role:{type:String,required:true,default:'user'},
